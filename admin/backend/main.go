cef2bdf8441b4c63fd9f32311e828d09a0b21f13
InnoNodo <k.nosov@innopolis.university>
2025-06-11 18:08:27 +0300

    Added new method in admin backend

--- admin/backend/main.go
+++ admin/backend/main.go
@@ -10,7 +10,12 @@ func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World! %s", time.Now())
 }
 
+func newMethod(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("This is a new method.")
+}
+
 func main() {
 	http.HandleFunc("/", greet)
+	http.HandleFunc("/new_method", newMethod) 
 	http.ListenAndServe(":8080", nil)
 }
