bd5bca96265fe4cd233c268499b78f0b24879ac4
InnoNodo <k.nosov@innopolis.university>
2025-06-17 15:03:11 +0300

    Added an architecture for Go project

---
+++ admin/backend/app/routes.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func CombineRoutes(app *fiber.App) {
+	// authorization.SetupAuthRoutes(app)
+	// registration.SetupRegistrationRoutes(app)
+	// resource.SetupResourceRoutes(app)
+	// bookings.SetupBookingRoutes(app)
+	// events.SetupEventRoutes(app)
+	// slots.SetupSlotsRoutes(app)
+}
