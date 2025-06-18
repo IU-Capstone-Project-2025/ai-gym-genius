b47466f72929fd0007301d26b8936b60f4b0144d
shara <sharapov.savely@yandex.ru>
2025-06-18 12:13:33 +0300

    add: users activity graph with time interval switcher button, based on static data

--- admin/frontend/pages/index.vue
+++ admin/frontend/pages/index.vue
@@ -1,29 +1,13 @@
 <template>
-  <div class="w-[400px]">
-    <UAccordion :items="items"/>
+  <div class="container mx-auto p-6 space-y-6">
+    <h1 class="text-3xl font-bold mb-8">Dashboard</h1>
+
+    <UserActivityDashboard/>
   </div>
 </template>
 
 <script setup lang="ts">
-import type {AccordionItem} from '@nuxt/ui'
-
-const items = ref<AccordionItem[]>([
-  {
-    label: 'Icons',
-    icon: 'i-lucide-smile',
-    content: 'You have nothing to do, @nuxt/icon will handle it automatically.'
-  },
-  {
-    label: 'Colors',
-    icon: 'i-lucide-swatch-book',
-    content: 'Choose a primary and a neutral color from your Tailwind CSS theme.'
-  },
-  {
-    label: 'Components',
-    icon: 'i-lucide-box',
-    content: 'You can customize components by using the `class` / `ui` props or in your app.config.ts.'
-  }
-])
+import UserActivityDashboard from '~/components/organisms/UserActivityDashboard.vue'
 </script>
 
 <style scoped>
