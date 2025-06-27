3f6bf53704a9ed47e82e28612a5890805767b642
shara <sharapov.savely@yandex.ru>
2025-06-27 18:04:37 +0300

    create UserActivityDashboard module instead of basic vue component

--- admin/frontend/pages/index.vue
+++ admin/frontend/pages/index.vue
@@ -7,7 +7,9 @@
 </template>
 
 <script setup lang="ts">
-import UserActivityDashboard from '~/components/organisms/UserActivityDashboard.vue'
+
+import {UserActivityDashboard} from "~/components/organisms/UserActivityDashboard";
+
 </script>
 
 <style scoped>
