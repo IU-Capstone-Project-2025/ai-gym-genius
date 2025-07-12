f02decca1ce4cf3f8332c3a4cc5f29328d55b004
InnoNodo <k.nosov@innopolis.university>
2025-07-12 15:15:07 +0300

    Added fields for the user

--- admin/backend/internal/database/schemas/user.go
+++ admin/backend/internal/database/schemas/user.go
@@ -1,10 +1,20 @@
 package schemas
 
-// TODO add other fields: email, phone, or whatever
+import "time"
 
 type User struct {
 	ID    uint   `gorm:"primaryKey"`
 	Login string `gorm:"uniqueIndex;not null"`
+	Name  string `gorm:"not null"`
+	Surname string `gorm:"not null"`
+	Email string `gorm:"not null;uniqueIndex"`
+	SubscriptionType string `gorm:"not null"`
+	Status string `gorm:"not null"` // e.g., "active", "inactive", "banned"
+	LastActivity time.Time `gorm:"not null"` // e.g., timestamp of last activity
+	NumberOfWorkouts int `gorm:"not null"` // e.g., number of workouts completed
+	TotalTimeSpent time.Duration `gorm:"not null"` // e.g., total time spent in workouts
+	StreakCount int `gorm:"not null"` // e.g., number of consecutive workouts
+	AverageWorkoutDuration time.Duration `gorm:"not null"` // e.g., average duration of workouts
 	Hash  string `gorm:"not null"`
 }
 
