8064f043009f220f1ebe792fb576151b7cf02930
shara <sharapov.savely@yandex.ru>
2025-07-17 19:04:36 +0300

    add: connected index page (users statistics) to the backend

--- admin/frontend/composables/useUsersStats.ts
+++ admin/frontend/composables/useUsersStats.ts
@@ -1,13 +1,15 @@
+import { ref, type Ref } from 'vue'
+import { useUserStore } from '~/stores/user'
+
 type UsersStats = {
     start_date: string, // Date like
     end_date: string,
     count: number
 }
 
-const usersStats: Ref<UsersStats[]> = ref([]);
-const userStore = useUserStore();
-
 export const useUsersStats = () => {
+    const usersStats: Ref<UsersStats[]> = ref([]);
+    const userStore = useUserStore();
     const startDate = '2023-06-01T00:00:00Z';
     const endDate = '2025-06-10T00:00:00Z'
 
